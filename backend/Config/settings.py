c55474c5ed2615e54288e0c627082943017100b1
nelson chege <nelson3chege@gmail.com>
2022-05-02 13:29:18 +0300

    (update):intergrate postgres db

--- backend/Config/settings.py
+++ backend/Config/settings.py
@@ -78,9 +78,14 @@ WSGI_APPLICATION = 'Config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testproject',
+        'USER': 'postgres',
+        'PASSWORD': 'wslpassword2',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
