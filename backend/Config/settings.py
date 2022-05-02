b34d71d7040e783cead854b63b890505ffbfdd6a
nelson chege <nelson3chege@gmail.com>
2022-05-02 13:16:19 +0300

    (update):connecting frontend and backend

--- backend/Config/settings.py
+++ backend/Config/settings.py
@@ -103,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ALLOW_ALL_ORIGINS: True
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -115,7 +114,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
